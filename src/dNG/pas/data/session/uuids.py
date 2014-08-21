138fc3bbbf4098ab2e4342903dacfb8c53e57704
NotTheEvilOne <git-NTEO@vplace.de>
2014-08-21 20:25:34 +0200

    Trivial: Removed file extension from URLs in the file header

--- src/dNG/pas/data/session/uuids.py
+++ src/dNG/pas/data/session/uuids.py
@@ -6,13 +6,13 @@ direct PAS
 Python Application Services
 ----------------------------------------------------------------------------
 (C) direct Netware Group - All rights reserved
-http://www.direct-netware.de/redirect.py?pas;session
+https://www.direct-netware.de/redirect?pas;session
 
 This Source Code Form is subject to the terms of the Mozilla Public License,
 v. 2.0. If a copy of the MPL was not distributed with this file, You can
 obtain one at http://mozilla.org/MPL/2.0/.
 ----------------------------------------------------------------------------
-http://www.direct-netware.de/redirect.py?licenses;mpl2
+https://www.direct-netware.de/redirect?licenses;mpl2
 ----------------------------------------------------------------------------
 #echo(pasSessionVersion)#
 #echo(__FILEPATH__)#
@@ -42,7 +42,7 @@ session implementation.
 :package:    pas
 :subpackage: session
 :since:      v0.1.00
-:license:    http://www.direct-netware.de/redirect.py?licenses;mpl2
+:license:    https://www.direct-netware.de/redirect?licenses;mpl2
              Mozilla Public License, v. 2.0
 	"""
 
